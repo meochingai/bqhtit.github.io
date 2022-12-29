695e00b0364121c213498c3d9bb5295fbc187d4d
Tít ở trên github <115094709+bqhtit@users.noreply.github.com>
2022-12-29 20:29:23 +0700

    Update style.js

--- style.js
+++ style.js
@@ -1,12 +1,1 @@
-$('.upload').on('click touch', function(e) {
 
-    e.preventDefault();
-
-    var self = $(this);
-
-    self.addClass('loading');
-    setTimeout(function() {
-        self.removeClass('loading');
-    }, 4200)
-
-});
